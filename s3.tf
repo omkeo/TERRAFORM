24418729c21dc190203537627c04d4dddc2a8b03
Omkar Gunjal <omkarg3937@gmail.com>
2025-05-14 13:00:22 +0530

    terra added

--- s3.tf
+++ s3.tf
@@ -2,7 +2,7 @@ provider "aws" {
       region = "ap-south-1"
   }
 
-resource "aws_s3_bucket" "myterraform" {
+resource "aws_s3_bucket" "my-specical-2134" {
     bucket = "123-newbucket"
 
 tags = {

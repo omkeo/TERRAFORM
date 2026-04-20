91f82cf03dcf5c1408482841f64f1c16ecc29d10
omkar <omkar123@gmail.com>
2026-04-20 12:51:14 +0530

    --- Auto Git Commit ---

--- VPC_EC2/vpc_ec2.tf
+++ VPC_EC2/vpc_ec2.tf
@@ -88,7 +88,7 @@ resource "aws_security_group" "web_sg" {
 # EC2 with Nginx
 resource "aws_instance" "my_ec2" {
   ami                    = "ami-05d2d839d4f73aafb"
-  instance_type          = "t2.micro"
+  instance_type          = "t3.micro"
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
